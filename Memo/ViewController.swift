ec67dbc0fe0d4af33ac1f776bd2ee1e78c2122c9
George Gomes <>
2021-03-06 00:00:05 -0300

    Create proj

---
+++ Memo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by George Vilnei Arboite Gomes on 05/03/21.
+//  Copyright © 2021 George Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
